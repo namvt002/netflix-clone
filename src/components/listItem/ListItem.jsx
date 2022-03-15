78ba0a4cd6ef1998dbaa639bcef2149640a94824
namvt002 <viettrung0601@gmail.com>
2022-03-15 16:50:17 +0700

    107

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./listItem.scss"
+
+const Listitem = () => {
+    return (
+        <div className="listItem">
+            item
+        </div>
+    );
+}
+
+export default Listitem;
